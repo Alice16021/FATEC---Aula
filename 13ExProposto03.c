7f3efca786ae177cdc9c46f4b83e818ba4b66f59
Alice Maria <72365221+Alice16021@users.noreply.github.com>
2026-09-02 20:27:32 -0300

    Refactor Celsius to Fahrenheit conversion function

--- 13ExProposto03.c
+++ 13ExProposto03.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-float celsius
-float celsiusParaFahrenheit = (celsius * 1.8) + 32;
+float celsiusParaFahrenheit(float celsius) {
+    return (celsius * 1.8) + 32;;
+}
 
 int main() {
-    float temperatura1 = 100.0, temperatura2 = 125.5, temperatura3 = 300.0;
+    float temperatura1 = 100.0;
+    float temperatura2 = 125.5;
+    float temperatura3 = 300.0;
 
-    printf("Leitura do sensor de Celsius para Fahrenheit \n");
+    printf("Leitura do sensor de Celsius para Fahrenheit:\n");
     printf("Temp 1: %.2f C = %.2f F\n", temperatura1, celsiusParaFahrenheit(temperatura1));
     printf("Temp 2: %.2f C = %.2f F\n", temperatura2, celsiusParaFahrenheit(temperatura2));
     printf("Temp 3: %.2f C = %.2f F\n", temperatura3, celsiusParaFahrenheit(temperatura3));
