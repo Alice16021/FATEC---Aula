c364c0a8c09621210ea944a862b9b5cc23675629
Alice Maria <72365221+Alice16021@users.noreply.github.com>
2026-09-02 20:20:55 -0300

    Fix celsiusToFahrenheit function and variable names

--- 13ExProposto03.c
+++ 13ExProposto03.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-
-float celsiusParaFahrenheit(float celsius) {
-    return (celsius * 1.8) + 32;
-}
-
+float celsius
+float celsiusParaFahrenheit = (celsius * 1.8) + 32;
 
 int main() {
-    float temp1 = 100.0, temp2 = 125.5, temp3 = 300.0;
+    float temperatura1 = 100.0, temperatura2 = 125.5, temperatura3 = 300.0;
 
     printf("Leitura do sensor de Celsius para Fahrenheit \n");
-    printf("Temp 1: %.2f C = %.2f F\n", temp1, celsiusParaFahrenheit(temp1));
-    printf("Temp 2: %.2f C = %.2f F\n", temp2, celsiusParaFahrenheit(temp2));
-    printf("Temp 3: %.2f C = %.2f F\n", temp3, celsiusParaFahrenheit(temp3));
+    printf("Temp 1: %.2f C = %.2f F\n", temperatura1, celsiusParaFahrenheit(temperatura1));
+    printf("Temp 2: %.2f C = %.2f F\n", temperatura2, celsiusParaFahrenheit(temperatura2));
+    printf("Temp 3: %.2f C = %.2f F\n", temperatura3, celsiusParaFahrenheit(temperatura3));
 
     return 0;
 }
