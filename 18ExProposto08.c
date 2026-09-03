3f116746dcc3b2d4ae9798b158a8a3b0df0ea550
Alice Maria <72365221+Alice16021@users.noreply.github.com>
2026-09-02 21:10:40 -0300

    Refactor to inline find max in main function

--- 18ExProposto08.c
+++ 18ExProposto08.c
@@ -1,27 +1,10 @@
 #include <stdio.h>
 
-int encontrarMaior(int matriz[3][3], int *linha, int *coluna) {
-    int i, j;
-    int maior = matriz[0][0];
-    *linha = 0;
-    *coluna = 0;
-
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (matriz[i][j] > maior) {
-                maior = matriz[i][j];
-                *linha = i;
-                *coluna = j;
-            }
-        }
-    }
-    return maior;
-}
-
 int main() {
     int mat[3][3];
-    int i, j, maiorVal, lin, col;
-
+    int i, j;
+    int maior, lin, col;
+    
     printf("Digite os elementos da matriz 3x3:\n");
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
@@ -30,10 +13,23 @@ int main() {
         }
     }
 
-    maiorVal = encontrarMaior(mat, &lin, &col);
+    maior = mat[0][0];
+    lin = 0;
+    col = 0;
 
-    printf("\n Maior valor encontrado: %d \n", maiorVal);
-    printf("Localizacao: Linha %d, Coluna %d \n", lin, col);
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (mat[i][j] > maior) {
+                maior = mat[i][j];
+                lin = i;
+                col = j;
+            }
+        }
+    }
+
+    
+    printf("\nMaior valor encontrado: %d\n", maior);
+    printf("Localizacao: Linha %d, Coluna %d\n", lin, col);
 
     return 0;
 }
